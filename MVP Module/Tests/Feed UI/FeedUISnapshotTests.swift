a77841afff8ab15f6ecba0189b214f9b1850dac6
Luis Zapata <lalejzapata@falabella.cl>
2021-09-04 17:20:35 -0400

    Feed View loads properly with error

--- MVP Module/Tests/Feed UI/FeedUISnapshotTests.swift
+++ MVP Module/Tests/Feed UI/FeedUISnapshotTests.swift
@@ -23,15 +23,14 @@ class FeedUISnapshotTests: XCTestCase {
 		assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_FEED_dark")
 	}
 
-//
-//	func test_feedWithError() {
-//		let sut = makeSUT()
-//
-//		sut.display(errorMessage: "An error message")
-//
-//		assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_ERROR_light")
-//		assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_ERROR_dark")
-//	}
+	func test_feedWithError() {
+		let sut = makeSUT()
+
+		sut.display(errorMessage: "An error message")
+
+		assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_ERROR_light")
+		assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_ERROR_dark")
+	}
 
 	// MARK: - Helpers
 
